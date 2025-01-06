65eb30b3aa5aa58f81e2f545c88c0ad8a154e3c9
Hassan <hasan-account2012-3@live.com>
2025-01-06 23:03:19 +0100

    update graph example

--- visualize.cpp
+++ visualize.cpp
@@ -1,20 +1,42 @@
 #include "agrad/Value.hpp"
 #include "agrad/ValueGraph.hpp"
 
+// int main(int argc, char *argv[])
+// {
+//     auto a = Value::create(1.0, "a");
+//     auto b = Value::create(2.0, "b");
+//     auto c = Value::create(3.0, "c");
+
+//     auto g = a * b;
+//     auto f = g * c;
+
+//     g->setLabel("g");
+//     f->setLabel("f");
+//     f->backward();
+
+//     ValueGraph::visualize(f.get(), "graph");
+
+//     return 0;
+// }
 int main(int argc, char *argv[])
 {
-    auto a = Value::create(1.0, "a");
-    auto b = Value::create(2.0, "b");
-    auto c = Value::create(3.0, "c");
+    auto x1 = Value::create(2.0, "x1");
+    auto x2 = Value::create(0.0, "x2");
 
-    auto g = a * b;
-    auto f = g * c;
+    // Create a simple neural network computation
+    auto w1 = Value::create(-3.0, "w1");
+    auto w2 = Value::create(1.0, "w2");
+    auto b = Value::create(6.8813735870195432, "b");
 
-    g->setLabel("g");
-    f->setLabel("f");
-    f->backward();
+    auto x1w1 = x1 * w1;
+    auto x2w2 = x2 * w2;
+    auto sum = x1w1 + x2w2 + b;
+    auto o = sum->tanh();
 
-    ValueGraph::visualize(f.get(), "graph");
+    o->setLabel("output");
+    o->backward();
+
+    ValueGraph::visualize(o.get(), "graph");
 
     return 0;
 }
